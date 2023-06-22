6099b6550c817904508dd8372d9d24ea58fd551b
roya232 <137366431+roya232@users.noreply.github.com>
2023-06-22 02:44:51 +0000

    added user age question

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Roya")
-user_name = input("What is your name:  ")
+print("welcome")
+user_name = input("What is your name?  ")
 print(f"Hello {user_name}")
-#user_age = int(input)
+user_age = int(input("How old are you?  "))
+print(f"You are {user_age} years old")
+if user_age >= 20:
+  print("Wow you are old")
+else:
+  print("Gee you are young!")
